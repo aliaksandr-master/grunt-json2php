ecdea4a9eb7635c6c1bc6d50eb140f4179168e92
Aliaksandr <aliaksandr.pasynkau@sport.com>
2015-01-05 15:34:04 +0300

    add alpha version of logic

---
+++ tasks/index.js
@@ -0,0 +1,33 @@
+'use strict';
+
+var path = require('path');
+var convert = require('./../lib');
+
+module.exports = function (grunt) {
+	grunt.registerMultiTask('excel_vocabulary', function () {
+		var options = this.options({
+            root: process.cwd(),
+
+			cover: function (phpArrayString, dest) {
+				return '<?php\n\nreturn ' + phpArrayString + ';\n\n?>';
+			}
+		});
+
+		this.files.forEach(function (f) {
+			f.src.filter(function (filepath) {
+				if (!grunt.file.exists(filepath)) {
+					grunt.log.warn('Source file "' + filepath + '" not found.');
+					return false;
+				}
+
+                return true;
+			}).forEach(function (srcFilePath) {
+				var json = grunt.file.readJSON(path.resolve(options.root, srcFilePath));
+				var phpArrayString = convert(json, options);
+
+				grunt.file.write(f.dest, options.cover(phpArrayString, f.dest));
+				grunt.log.writeln('File "' + f.dest + '" created.');
+			});
+		});
+	});
+};
