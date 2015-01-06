e5c4652f783dea42ea7c436ed03c371f213510f9
Aliaksandr <aliaksandr.pasynkau@sport.com>
2015-01-06 12:04:58 +0300

    add correct tests for simple mode and compress mode

--- tests/index.js
+++ tests/index.js
@@ -1,19 +1,18 @@
 
 "use strict";
 
-var parse = require('./lib');
+var convert = require('./lib');
 
-module.exports['file with one empty sheet'] = function (test) {
-	test.ok(1);
+var object = {z: undefined, y: function () {}, a: 1, b: {}, c: "3", d: null, e: true, f: false, g: [], h: [1, "2", true, false, null], i: { a: 1, b: "2", c: true, d: false, e: null}};
+var objectPhp = 'array("a" => 1, "b" => array(), "c" => "3", "d" => null, "e" => true, "f" => false, "g" => array(), "h" => array(1, "2", true, false, null), "i" => array("a" => 1, "b" => "2", "c" => true, "d" => false, "e" => null))';
+var objectPhpCompact = 'array("a"=>1,"b"=>array(),"c"=>"3","d"=>null,"e"=>true,"f"=>false,"g"=>array(),"h"=>array(1,"2",true,false,null),"i"=>array("a"=>1,"b"=>"2","c"=>true,"d"=>false,"e"=>null))';
+
+module.exports['big object'] = function (test) {
+	test.deepEqual(convert(object), objectPhp);
 	test.done();
 };
 
-module.exports['file with one non empty sheet'] = function (test) {
-
-	test.done();
-};
-
-module.exports['file with two non empty sheet'] = function (test) {
-
+module.exports['empty'] = function (test) {
+	test.deepEqual(convert(object, {compress: true}), objectPhpCompact);
 	test.done();
 };
